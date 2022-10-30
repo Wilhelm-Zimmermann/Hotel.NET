1eb841eb0dbb49b75ea5bb7c8fa4a938a8f6f1eb
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2022-10-30 17:06:27 -0300

    Using a real database (Postgres) with migrations

--- Hotel.Domain.Infra/Contexts/DatabaseContext.cs
+++ Hotel.Domain.Infra/Contexts/DatabaseContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Hotel.Domain.Entities;
+using Microsoft.Extensions.Configuration;
 
 namespace Hotel.Domain.Infra.Contexts
 {
@@ -9,12 +10,21 @@ namespace Hotel.Domain.Infra.Contexts
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-
+           
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            optionsBuilder.UseInMemoryDatabase("Data");
+            modelBuilder.Entity<HotelGuest>(entity =>
+            {
+                entity.ToTable("hotel_guests");
+                entity.Property(x => x.Id);
+                entity.Property(x => x.Name);
+                entity.Property(x => x.BirthDate);
+                entity.Property(x => x.Email);
+                entity.Property(x => x.PhoneNumber);
+                entity.Property(x => x.Om);
+            });
         }
     }
 }
---
+++ Hotel.Domain.Infra/Migrations/20221029223842_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Domain.Infra.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "hotel_guests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Om = table.Column<string>(type: "text", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hotel_guests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "hotel_guests");
+        }
+    }
+}
